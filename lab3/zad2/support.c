1b00ab8c7408861dad14bdbc2de3d6d64c53f03d
mikeyzat@gmail.com <mikeyzat@gmail.com>
2020-03-22 15:17:53 +0100

    Add tests to support and makefile

--- lab3/zad2/support.c
+++ lab3/zad2/support.c
@@ -1,6 +1,9 @@
+#define MAX_COLS_NUMBER 1000
+#define MAX_LINE_LENGTH (MAX_COLS_NUMBER * 5)
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 int random_inclusive(int min, int max)
 {
@@ -25,12 +28,147 @@ void generate_matrix(int rows, int cols, char *filename)
   }
 }
 
+typedef struct
+{
+  int **values;
+  int rows;
+  int cols;
+} matrix;
+
+int get_cols_number(char *row)
+{
+  int cols = 0;
+  char *tmp = row;
+  while ((tmp = strchr(tmp, ' ')) != NULL)
+  {
+    cols++;
+    tmp++;
+  }
+  return cols + 1;
+}
+
+void get_dimensions(FILE *matrix_file, int *rows, int *cols)
+{
+  char line[MAX_LINE_LENGTH];
+  *rows = 0;
+  *cols = 0;
+
+  while (fgets(line, MAX_LINE_LENGTH, matrix_file) != NULL)
+  {
+    if (*cols == 0)
+    {
+      *cols = get_cols_number(line);
+    }
+
+    *rows = *rows + 1;
+  }
+
+  fseek(matrix_file, 0, SEEK_SET);
+}
+
+matrix load_matrix(char *filename)
+{
+  FILE *matrix_file = fopen(filename, "r");
+
+  int rows, cols;
+  get_dimensions(matrix_file, &rows, &cols);
+
+  int **values = calloc(rows, sizeof(int *));
+  for (int y = 0; y < rows; y++)
+  {
+    values[y] = calloc(cols, sizeof(int));
+  }
+
+  int x_curr, y_curr = 0;
+  char line[MAX_LINE_LENGTH];
+  while (fgets(line, MAX_LINE_LENGTH, matrix_file) != NULL)
+  {
+    x_curr = 0;
+    char *encoded_number = strtok(line, " ");
+    while (encoded_number != NULL)
+    {
+      values[y_curr][x_curr++] = atoi(encoded_number);
+      encoded_number = strtok(NULL, " ");
+    }
+
+    y_curr++;
+  }
+
+  fclose(matrix_file);
+
+  matrix retval;
+  retval.values = values;
+  retval.rows = rows;
+  retval.cols = cols;
+
+  return retval;
+}
+
+void test()
+{
+  matrix a = load_matrix("a.txt");
+  matrix b = load_matrix("b.txt");
+  matrix c = load_matrix("c.txt");
+  int **first = a.values;
+  int **second = b.values;
+  int **third = c.values;
+  int **result = calloc(a.rows, sizeof(int *));
+  for (int i = 0; i < a.rows; i++) // init result matrix
+  {
+    result[i] = calloc(b.cols, sizeof(int));
+    for (int j = 0; j < b.cols; j++)
+    {
+      result[i][j] = 0;
+    }
+  }
+
+  for (int c = 0; c < a.rows; c++)
+  {
+    for (int d = 0; d < b.cols; d++)
+    {
+      int sum = 0;
+      for (int k = 0; k < b.rows; k++)
+      {
+        sum += first[c][k] * second[k][d];
+      }
+      result[c][d] = sum;
+    }
+  }
+  for (int i = 0; i < a.rows; i++) // init result matrix
+  {
+    for (int j = 0; j < b.cols; j++)
+    {
+      if (result[i][j] != third[i][j])
+      {
+        printf("Incorrect result of multiplying matrixes\n\
+         %d != %d at index %d %d\n",
+               result[i][j], third[i][j], i, j);
+      }
+    }
+  }
+  printf("Correct result of multypling the matrixes");
+}
+
 int main(int argc, char *argv[])
 {
-  if (argc != 3)
+  if (argc != 3 && argc != 2)
   {
-    fprintf(stderr, "Usage: ./helper min max");
+    fprintf(stderr, "Usage: ./helper min max or ./helper test");
     return 1;
+  }
+
+  if (argc == 2)
+  {
+    if (strcmp(argv[1], "test") == 0)
+    {
+      test();
+    }
+    else
+    {
+      fprintf(stderr, "Usage: ./helper min max or ./helper test");
+      return 1;
+    }
+    return 0;
   }
 
   srand(time(NULL));
